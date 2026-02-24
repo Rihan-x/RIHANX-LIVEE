ab45a36c74f86dd7b443cb8d8fe5fc4e4cdf83ef
SHEIKH RIHAN <skgamar875@gmail.com>
2026-02-24 21:50:31 +0600

    Add files via upload

--- js/youtube-api.js
+++ js/youtube-api.js
@@ -1,1 +1,17 @@
 
+const apiKey = "AIzaSyCbfAyIJ2YuF_NjJs4ai68sPDVU9v-OQUo";
+const channelId = "UCw838b6jU39JdlmAa7pUBUQ";
+
+async function fetchYouTubeData() {
+  const response = await fetch(
+    `https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${channelId}&key=${apiKey}`
+  );
+  const data = await response.json();
+  const stats = data.items[0].statistics;
+
+  document.getElementById("subscribers").innerText = stats.subscriberCount;
+  document.getElementById("views").innerText = stats.viewCount;
+}
+
+fetchYouTubeData();
+setInterval(fetchYouTubeData, 10000);
